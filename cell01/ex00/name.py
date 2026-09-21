3be1bb5cc04e60aa4f51c0a8abf9a9c901260eb9
Marax-0 <work.ratchann@gmail.com>
2026-09-21 16:36:05 +0700

    add2

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Ratchanon"
+last_name = "Phaleephuy"
+print(first_name + " " + last_name)
---
+++ cell01/name.py
@@ -0,0 +1,4 @@
+first_name = "Ratchanon"
+last_name = "Phaleephuy"
+whole_name = first_name + " " + last_name
+print(whole_name)
