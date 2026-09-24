b3c0cbddb03619409f441012dd4aae9d5207e4fd
Marax-0 <work.ratchann@gmail.com>
2026-09-24 13:43:57 +0700

    addkub

---
+++ miniproject/checkmate.py
@@ -0,0 +1,63 @@
+def checkmate(board):
+    if not board:
+        return
+
+    lines = [] 
+    for line in board.splitlines():
+        if line:
+            lines.append(line)
+            
+    if not lines:
+        return
+    
+    size = len(lines)
+    for line in lines:
+        if len(line) != size:
+            return
+
+    king_position = None
+    king_count = 0
+    for r in range(size):
+        for c in range(size):
+            if lines[r][c] == 'K':
+                king_position = (r,c)
+                king_count += 1
+
+    if king_count != 1:
+        return
+    kr,kc = king_position
+
+    pawn_check = [(kr+1,kc-1),(kr+1,kc+1)]
+    for pr,pc in pawn_check:
+        if 0 <= pr < size and 0 <= pc < size:
+            if lines[pr][pc] == 'P':
+                print("Success")
+                return
+
+    straight_direct = [(-1,0),(1,0),(0,-1),(0,1)]
+    for dr,dc in straight_direct:
+        r,c = kr+dr,kc+dc
+        while 0 <= r < size and 0 <= c < size:
+            piece = lines[r][c]
+            if piece in ('R','Q'):
+                print("Success")
+                return
+            elif piece != '.':
+                break
+            r += dr
+            c += dc
+
+    diago_direct = [(-1,-1),(-1,1),(1,1),(1,-1)]
+    for dr,dc in diago_direct:
+        r,c = kr+dr,kc+dc
+        while 0 <= r < size and 0 <= c < size:
+            piece = lines[r][c]
+            if piece in ('Q','B'):
+                print("Success")
+                return
+            elif piece != '.':
+                break
+            r += dr
+            c += dc
+
+    print("Fail")
